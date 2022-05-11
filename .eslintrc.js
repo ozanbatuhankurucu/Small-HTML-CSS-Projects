07d1d4c990c49174fb0dc8b6ff65a2c41acd5917
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-05-11 13:17:25 +0300

    Add airbnb style, eslint, prettier configs.

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    semi: [2, 'never'],
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', 'tsx'] }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'jsx-quotes': [2, 'prefer-single'],
+    'comma-dangle': [
+      true,
+      {
+        singleline: 'never',
+        multiline: {
+          objects: 'always',
+          arrays: 'always',
+          functions: 'never',
+          typeLiterals: 'ignore'
+        }
+      }
+    ]
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 function App() {
+  const a = 2
+  const as = {
+    asfsafasfafsa: 1,
+    asfasfasfasfasfasf: 2,
+    asfasfsafsafsafasfasfasf: 2,
+  }
   return <div>Hello World!</div>
 }
 
