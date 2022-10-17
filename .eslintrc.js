30fa43a676ca790dcc02850ae5bd13c51cfbe7f3
ozanbatuhankurucu <ozanbatusss@gmail.com>
2022-10-17 13:40:23 +0300

    Add form structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,6 +60,22 @@ module.exports = {
     'implicit-arrow-linebreak': 'off',
     multiline: 'off',
     quotes: 'off',
-    'function-paren-newline': 'off'
+    'function-paren-newline': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id']
+        }
+      }
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id']
+        }
+      }
+    ]
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ module.exports = {
         'left-bg-color': 'rgba(87, 84, 236, 0.7)',
         'right-bg-color': 'rgba(43, 43, 43, 0.8)',
         'left-btn-hover-color': 'rgba(87, 84, 236, 1)',
-        'right-btn-hover-color': 'rgba(28, 122, 28, 1)'
+        'right-btn-hover-color': 'rgba(28, 122, 28, 1)',
+        steelblue: '#4682B4',
+        'black-400': 'rgba(0, 0, 0, 0.4)'
       },
       backgroundImage: {
         ps: "url('/src/assets/images/ps.png')",
         xbox: "url('/src/assets/images/xbox.png')"
+      },
+      fontFamily: {
+        muli: ['Muli', 'sans-serif']
       }
     }
   },
