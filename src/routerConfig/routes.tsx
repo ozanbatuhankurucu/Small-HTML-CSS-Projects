efe24e159af871135caec1e464c092645349a01f
ozanbatuhankurucu <ozanbatusss@gmail.com>
2023-02-19 15:38:25 +0300

    Add unique id to the routes

--- src/routerConfig/routes.tsx
+++ src/routerConfig/routes.tsx
@@ -12,65 +12,54 @@ import DiamondAutograder from '../projects/DiamondAutograder'
 import DutyCycleSamplingWrapper from '../projects/DutyCycleSampling'
 import { Results } from '../projects/DutyCycleSampling/Results'
 import { Inputs } from '../projects/DutyCycleSampling/Inputs'
+import { generateUniqueKey } from '../utils'
 
 const routes = [
   {
-    id: '0',
     path: '/',
     element: <Navigate to='/projects' />
   },
   {
-    id: '1',
     path: '/projects/',
     element: <Projects />
   },
   {
-    id: '2',
     path: '/projects/hidden-search-widget',
     element: <HiddenSearchWidget />
   },
   {
-    id: '3',
     path: '/projects/blurry-loading',
     element: <BlurryLoading />
   },
   {
-    id: '4',
     path: '/projects/scroll-animation',
     element: <ScrollAnimation />
   },
   {
-    id: '5',
     path: '/projects/split-landing-page',
     element: <SplitLandingPage />
   },
   {
-    id: '6',
     path: '/projects/form-wave-animation',
     element: <Wrapper />
   },
   {
-    id: '7',
     path: '/projects/sound-board',
     element: <SoundBoard />
   },
   {
-    id: '8',
     path: '/projects/search-dropdown',
     element: <SearchDropdown />
   },
   {
-    id: '9',
     path: '/projects/dad-jokes',
     element: <DadJokes />
   },
   {
-    id: '10',
     path: '/projects/diamond-autograder',
     element: <DiamondAutograder />
   },
   {
-    id: '11',
     path: '/projects/duty-cycle-sampling',
     element: <DutyCycleSamplingWrapper />,
     childRoutes: (
@@ -82,4 +71,10 @@ const routes = [
     )
   }
 ]
-export default routes
+
+const routesWithId = routes.map((route) => ({
+  ...route,
+  id: generateUniqueKey(route.path)
+}))
+
+export default routesWithId
