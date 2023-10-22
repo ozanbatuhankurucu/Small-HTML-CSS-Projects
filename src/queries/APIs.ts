051f14c85c5eef10325ce18c20945fc5b5818f09
ozanbatuhankurucu <ozanbatusss@gmail.com>
2023-10-22 21:33:40 +0300

    Refactor endpoints

--- src/queries/APIs.ts
+++ src/queries/APIs.ts
@@ -1,7 +1,18 @@
+import endpoints from './endpoints'
+
 export async function fetchMovies() {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&page=1`
-  )
+  const response = await fetch(endpoints.MOVIE.DISCOVER)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = await response.json()
+  return data.results
+}
+
+export async function searchMovie(query: string) {
+  const response = await fetch(endpoints.MOVIE.SEARCH(query))
 
   if (!response.ok) {
     throw new Error('Failed to fetch data')
