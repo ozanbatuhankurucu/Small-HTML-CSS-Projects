8791c230922e933a8cd211ed6c70cb0e75f0d4c4
ozanbatuhankurucu <ozanbatusss@gmail.com>
2023-10-22 21:34:30 +0300

    Refactor

--- src/projects/MovieApp/components/MovieCard.tsx
+++ src/projects/MovieApp/components/MovieCard.tsx
@@ -5,17 +5,19 @@ interface MovieCardProps {
   movie: Movie
 }
 
-export const MovieCard: FC<MovieCardProps> = ({ movie }) => (
+export const MovieCard: FC<MovieCardProps> = ({
+  movie: { poster_path: posterPath, title, vote_average: voteAverage }
+}) => (
   <div className='rounded-lg rounded-tl-none overflow-hidden shadow-lg bg-[#373b69] relative'>
     <img
-      src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`}
-      alt={movie.title}
-      className='w-full h-[450px] object-cover'
+      src={`https://image.tmdb.org/t/p/w1280${posterPath}`}
+      alt={title}
+      className='w-full'
     />
     <div className='p-4'>
-      <h3 className='text-white text-lg'>{movie.title}</h3>
+      <h3 className='text-white text-lg'>{title}</h3>
       <div className='bg-[#22254b] text-orange-400 p-2 absolute top-0 left-0 rounded-br-md border border-[#373b69] shadow-sm'>
-        {movie.vote_average.toFixed(1)}
+        {voteAverage.toFixed(1)}
       </div>
     </div>
   </div>
--- src/projects/MovieApp/components/MovieList.tsx
+++ src/projects/MovieApp/components/MovieList.tsx
@@ -6,7 +6,7 @@ export const MovieList = () => {
   const { data: movies } = useMoviesData()
 
   return (
-    <div className='grid grid-cols-4 gap-8 bg-[#22254b] p-4'>
+    <div className='grid grid-cols-1 gap-8 bg-[#22254b] p-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
       {movies?.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
