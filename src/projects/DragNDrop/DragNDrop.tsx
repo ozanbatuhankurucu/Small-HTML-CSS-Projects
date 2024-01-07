1f4239d91c3ef85878f1c73d56653bc967454d2d
ozanbatuhankurucu <ozanbatusss@gmail.com>
2024-01-07 22:34:40 +0300

    Complete drag and drop project

--- src/projects/DragNDrop/DragNDrop.tsx
+++ src/projects/DragNDrop/DragNDrop.tsx
@@ -1,65 +1,69 @@
-import _ from 'lodash'
 import React, { useState } from 'react'
-import styled from 'styled-components'
-
-const Box = styled.div`
-  width: 200px;
-  padding: 10px;
-  margin: 10px;
-  border: 1px solid black;
-  background-color: #f0f0f0;
-`
+import cx from 'classnames'
 
 const DragNDrop = () => {
-  const [boxes, setBoxes] = useState<Record<string, string[]>>({
-    box_1: ['Item 1', 'Item 2', 'Item 3'],
-    box_2: ['Item 4', 'Item 5']
-  })
+  const [boxes, setBoxes] = useState([
+    'https://images.unsplash.com/photo-1682685797229-b2930538da47?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8',
+    '',
+    '',
+    '',
+    ''
+  ])
 
-  const [draggedItem, setDraggedItem] = useState<string>('')
-  const [sourceBox, setSourceBox] = useState<string>('')
+  const [draggedBoxIndex, setDraggedBoxIndex] = useState<number>(0)
+  const [hoveredBox, setHoveredBox] = useState<number | null>(null)
 
-  const onDragStart = (item: string, boxId: string) => {
-    setDraggedItem(item)
-    setSourceBox(boxId)
+  const onDragOver = (event: React.DragEvent) => {
+    event.preventDefault()
   }
 
-  const onDragOver = (event: React.DragEvent, boxId: string) => {
-    event.preventDefault()
-    if (boxId === sourceBox) {
-      // Optionally, you can provide visual feedback for invalid drop targets.
-      event.dataTransfer.dropEffect = 'none'
-    } else {
-      event.dataTransfer.dropEffect = 'move'
+  const onDragStart = (boxIndex: number) => {
+    setDraggedBoxIndex(boxIndex)
+  }
+
+  const onDrop = (boxIndex: number) => {
+    if (boxIndex !== draggedBoxIndex) {
+      setBoxes((prev) => {
+        const temp = [...prev]
+        temp[boxIndex] = temp[draggedBoxIndex]
+        temp[draggedBoxIndex] = ''
+        return temp
+      })
     }
   }
 
-  const boxesView = _.map(boxes, (items, boxId) => (
-    <Box
-      key={boxId}
-      onDragOver={(e) => onDragOver(e, boxId)}
-      onDrop={() => {
-        if (boxId !== sourceBox) {
-          setBoxes((prev) => ({
-            ...prev,
-            [sourceBox]: prev[sourceBox].filter((item) => item !== draggedItem),
-            [boxId]: [...prev[boxId], draggedItem]
-          }))
-        }
-        setDraggedItem('')
-        setSourceBox('')
-      }}>
-      {items.map((item) => (
-        <div key={item} draggable onDragStart={() => onDragStart(item, boxId)}>
-          {item}
+  const onDragEnter = (boxIndex: number) => {
+    setHoveredBox(boxIndex)
+  }
+
+  const boxesView = boxes.map((imgSrc, index) => (
+    <div
+      key={index}
+      className={cx('bg-white border-[2px] border-black w-24 h-24', {
+        '!bg-black !border-dashed !border-white':
+          hoveredBox === index && hoveredBox !== draggedBoxIndex
+      })}
+      onDragOver={onDragOver}
+      onDrop={() => onDrop(index)}
+      onDragEnter={() => onDragEnter(index)}>
+      {imgSrc && (
+        <div
+          className='h-full w-full'
+          draggable
+          onDragStart={() => onDragStart(index)}>
+          <img
+            className='h-full w-full object-cover'
+            src={imgSrc}
+            alt='box-img'
+          />
         </div>
-      ))}
-    </Box>
+      )}
+    </div>
   ))
 
   return (
     <div className='h-screen w-full flex justify-center items-center bg-steelblue text-black'>
-      {boxesView}
+      <div className='flex gap-4'>{boxesView}</div>
     </div>
   )
 }
