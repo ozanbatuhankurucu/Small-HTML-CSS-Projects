ef04385db1000b1e1bb14301d8045d8f83fd1719
ozanbatuhankurucu <ozanbatusss@gmail.com>
2024-03-31 15:36:27 +0300

    Add canvas element for drawing

--- src/projects/DrawingApp/DrawingApp.tsx
+++ src/projects/DrawingApp/DrawingApp.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Canvas from './components/Canvas'
 
 const DrawingApp = () => (
   <div className='h-screen w-full flex justify-center items-center'>
-    DrawingApp
+    <Canvas />
   </div>
 )
 export default DrawingApp
---
+++ src/projects/DrawingApp/components/Canvas.tsx
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from 'react'
+
+const Canvas: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+
+  const startDrawing = ({ nativeEvent }: React.MouseEvent) => {
+    const { offsetX, offsetY } = nativeEvent
+    const ctx = canvasRef.current?.getContext('2d')
+    if (ctx) {
+      ctx.beginPath()
+      ctx.moveTo(offsetX, offsetY)
+      setIsDrawing(true)
+    }
+  }
+
+  const draw = ({ nativeEvent }: React.MouseEvent) => {
+    if (!isDrawing) return
+    const { offsetX, offsetY } = nativeEvent
+    const ctx = canvasRef.current?.getContext('2d')
+    if (ctx) {
+      ctx.lineTo(offsetX, offsetY)
+      ctx.stroke()
+    }
+  }
+
+  const stopDrawing = () => {
+    const ctx = canvasRef.current?.getContext('2d')
+    if (ctx) {
+      ctx.closePath()
+      setIsDrawing(false)
+    }
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={startDrawing}
+      onMouseUp={stopDrawing}
+      onMouseMove={draw}
+      width={500}
+      height={500}
+      className='bg-gray-100'
+    />
+  )
+}
+
+export default Canvas
