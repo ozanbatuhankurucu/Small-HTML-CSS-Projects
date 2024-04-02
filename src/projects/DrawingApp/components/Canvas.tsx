515d860e524899bf7859bb6a0c9f3540de89d89c
ozanbatuhankurucu <ozanbatusss@gmail.com>
2024-04-02 13:45:44 +0300

    Add undoLastDrawing functionality

--- src/projects/DrawingApp/components/Canvas.tsx
+++ src/projects/DrawingApp/components/Canvas.tsx
@@ -1,8 +1,10 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Canvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [isDrawing, setIsDrawing] = useState(false)
+  // States for undo functionality
+  const [history, setHistory] = useState<ImageData[]>([])
 
   const startDrawing = ({ nativeEvent }: React.MouseEvent) => {
     const { offsetX, offsetY } = nativeEvent
@@ -32,11 +34,64 @@ const Canvas: React.FC = () => {
     }
   }
 
+  const saveState = () => {
+    const canvas = canvasRef.current
+    const ctx = canvas?.getContext('2d')
+    if (canvas && ctx) {
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+      setHistory([...history, imageData])
+    }
+  }
+
+  const undoLastDrawing = () => {
+    const canvas = canvasRef.current
+    const ctx = canvas?.getContext('2d')
+
+    setHistory((prevHistory) => {
+      if (prevHistory.length === 0) return prevHistory // Return early if history is empty
+
+      // Create a new array without the last item
+      const newHistory = prevHistory.slice(0, -1)
+
+      // Optionally restore the canvas state here if needed
+      if (canvas && ctx) {
+        if (newHistory.length > 0) {
+          const previousState = newHistory[newHistory.length - 1]
+          ctx?.putImageData(previousState, 0, 0)
+        } else {
+          // Clear canvas if no history
+          const canvas = canvasRef.current
+          const ctx = canvas?.getContext('2d')
+          ctx?.clearRect(0, 0, canvas.width, canvas.height)
+        }
+      }
+
+      return newHistory // Update the state with the new history
+    })
+  }
+
+  useEffect(() => {
+    const handleUndo = (event: KeyboardEvent) => {
+      if ((event.metaKey || event.ctrlKey) && event.key === 'z') {
+        undoLastDrawing()
+      }
+    }
+
+    window.addEventListener('keydown', handleUndo)
+
+    return () => {
+      window.removeEventListener('keydown', handleUndo)
+    }
+  }, [undoLastDrawing])
+
   return (
     <canvas
       ref={canvasRef}
       onMouseDown={startDrawing}
-      onMouseUp={stopDrawing}
+      onMouseUp={() => {
+        stopDrawing()
+        saveState()
+      }}
       onMouseMove={draw}
       width={500}
       height={500}
