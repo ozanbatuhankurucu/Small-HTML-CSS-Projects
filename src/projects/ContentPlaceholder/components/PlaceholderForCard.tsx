43dc0e7addfde068b4df8577c6c5a98d78c278df
ozanbatuhankurucu <ozanbatusss@gmail.com>
2024-06-21 16:39:25 +0300

    Update heights

--- src/projects/ContentPlaceholder/components/PlaceholderForCard.tsx
+++ src/projects/ContentPlaceholder/components/PlaceholderForCard.tsx
@@ -3,9 +3,9 @@ import ContentPlaceholder, { Item } from './ContentPlaceholder'
 export const PlaceholderForCard = () => {
   const placeholderItems: Item[] = [
     { height: '192px', width: '100%' },
-    { height: '28px', width: '60%', className: 'ml-6 mb-2 mt-4' },
-    { height: '22px', width: '90%', className: 'ml-6' },
-    { height: '22px', width: '80%', className: 'ml-6 mt-1' }
+    { height: '14px', width: '60%', className: 'ml-6 mb-2 mt-4' },
+    { height: '11px', width: '90%', className: 'ml-6' },
+    { height: '11px', width: '80%', className: 'ml-6 mt-1' }
   ]
 
   const avatarPlaceholderItems: Item[] = [
@@ -17,8 +17,8 @@ export const PlaceholderForCard = () => {
   ]
 
   const userInfoPlaceholderItems: Item[] = [
-    { height: '14px', width: '40%' },
-    { height: '16px', width: '40%', className: 'mt-1' }
+    { height: '8px', width: '40%' },
+    { height: '10px', width: '40%', className: 'mt-1' }
   ]
 
   return (
