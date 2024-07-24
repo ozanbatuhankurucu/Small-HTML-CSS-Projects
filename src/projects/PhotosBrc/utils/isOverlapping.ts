af0974270f67e6a16b4042d6a22ebbeaaa1af42c
ozanbatuhankurucu <ozanbatusss@gmail.com>
2024-07-24 12:11:35 +0300

    Improve isOverlapping function logic

--- src/projects/PhotosBrc/utils/isOverlapping.ts
+++ src/projects/PhotosBrc/utils/isOverlapping.ts
@@ -5,8 +5,8 @@ const isOverlapping = (
   photosParam: PhotoType[]
 ): boolean =>
   photosParam.some((photo) => {
-    const dx = photo.x - newPhoto.x
-    const dy = photo.y - newPhoto.y
+    const dx = photo.x + photo.size / 2 - (newPhoto.x + newPhoto.size / 2)
+    const dy = photo.y + photo.size / 2 - (newPhoto.y + newPhoto.size / 2)
     const distance = Math.sqrt(dx * dx + dy * dy)
     return distance < photo.size / 2 + newPhoto.size / 2
   })
